38ba5d48d8ef700fde619f1b1cdfbf4727748afa
Joan THOMAS <thomas.joan.pl@gmail.com>
2025-12-01 09:45:00 +0100

    feat: add Google Maps provider and base map component

---
+++ src/components/ui/map.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+/**
+ * Shared Google Maps exports — re-exports from @vis.gl/react-google-maps
+ * for convenient use throughout the app.
+ */
+export { Map, AdvancedMarker, InfoWindow, useMap } from "@vis.gl/react-google-maps";
+export { GoogleMapsProvider } from "./google-maps-provider";
